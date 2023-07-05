6b6759717da4319a18533036504381da283da5e6
Rojan Rajbhandari <rrojan20@gmail.com>
2023-07-05 17:51:49 +0545

    refactor: clean code

--- main.py
+++ main.py
@@ -1,44 +1,43 @@
-import os
+from utils import clear, get_filecount, calculate_eta
 import time
-
-
-def clear():
-    # for windows name is 'nt' while it is 'posix' for *nix mac / linux
-    clear_cmd = 'cls' if os.name == 'nt' else 'clear'
-    os.system(clear_cmd)
-
-
-def get_filecount(path):
-    count = 0
-    for _path in os.listdir(path):
-        if os.path.isfile(os.path.join(path, _path)) and _path != '.keep':
-            count += 1
-    return count
+import sys
 
 
 def main():
     MAX_ITERS = 99999999
     i = 0
 
+    start_time = time.time()
+    first_frame_created_time = start_time
+
     while i < MAX_ITERS:
         i += 1
 
         tmp_count = get_filecount('./tmp_frames')
         out_count = get_filecount('./out_frames')
-        completed = '{:.2f}'.format((out_count / tmp_count) * 100)
+
+        try:
+            completed = '{:.2f}'.format((out_count / tmp_count) * 100)
+        except ZeroDivisionError:
+            print('No frames in tmp_frames to process')
+            sys.exit(0)
+
+        eta = calculate_eta(first_frame_created_time, float(completed))
+
         clear()
         print(f'Current progress:', completed, '%')
         print(f'Completed frames:', out_count)
+        print(f'Time remaining:', eta)
 
-        # Break loop if 100%. If overshoots (some frame issue) then something is wrong
+        # Break loop if 100%. If overshoots (some frame issue), then something is wrong
         if completed == '100.00':
-            break
+            sys.exit(0)
         elif len(completed) >= 6:
             raise Exception('Tmp and out frame counts do not match')
 
         time.sleep(5)
 
-    raise Exception('Maximum framecount exceeded')
+    raise Exception('Maximum frame count exceeded')
 
 
 if __name__ == '__main__':
